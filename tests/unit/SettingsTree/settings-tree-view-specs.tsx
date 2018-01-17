89f29a5800d45751ad20bd61ef2fd857cb45178c
MAKARD <human.getEmail@gmail.com>
2018-01-17 19:01:39 +0200

    Add rest test suites

---
+++ tests/unit/SettingsTree/settings-tree-view-specs.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import {expect} from "chai";
+import {ReactWrapper, mount} from "enzyme";
+import {SettingsTreeView} from "../../../src/Components/SettingsTree/SettingsTreeView/SettingsTreeView";
+import {TableColumnRepository} from "../../../src/Components/TableColumn/TableColumnRepository";
+import {TableColumn} from "../../../src/Components/TableColumn/TableColumn";
+import {ColumnData} from "../helpers/ColumnData";
+
+describe("<SettingsTreeView/>", () => {
+    let wrapper: ReactWrapper<any>;
+
+    const commonHandler = () => undefined;
+    const repo = new TableColumnRepository().add(new TableColumn(ColumnData));
+
+    let saved;
+    const handleSave = () => {
+        saved = true;
+    };
+    let error;
+    const handleError = () => {
+        error = true;
+    }
+
+    const context = {
+        initData: commonHandler,
+        getCurrentVisibleData: commonHandler,
+        getColumn: commonHandler,
+        getCurrentData: commonHandler,
+        onError: handleError,
+        saveData: handleSave,
+    };
+
+    beforeEach(() => {
+        wrapper = mount(<SettingsTreeView childList={repo} activeTableKey="key"/>, {context})
+    })
+
+    afterEach(() => {
+        saved = false;
+        error = true;
+        wrapper.unmount();
+    })
+
+    it("Should call `context.saveData` on save data", () => {
+        (wrapper.instance() as any).handleSaveData();
+
+        expect(saved).to.be.true;
+    })
+
+    it("Should call `context.saveData` on move", () => {
+        (wrapper.instance() as any).handleMove(() => undefined)()
+
+        expect(saved).to.be.true;
+    })
+
+    it("Should call `context.onError` if error has been throwed on move", () => {
+        (wrapper.instance() as any).handleMove(() => {throw Error()})();
+
+        expect(error).to.be.true;
+    })
+})
+
+
