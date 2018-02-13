cbdcab79ddbb065a1445b39d9cfd938445806a8a
alexander <aleksa.yagich@gmail.com>
2018-02-13 09:48:24 +0200

    Remove code that is declared in parent component

--- src/Components/SearchTools/StringMatch/StringMatch.tsx
+++ src/Components/SearchTools/StringMatch/StringMatch.tsx
@@ -1,32 +1,12 @@
 import * as React from "react";
-import * as PropTypes from "prop-types";
 
-import { Condition, CriteriaTableContextTypes, CriteriaTableContext } from "../../CriteriaTable";
-import {
-    SearchToolRequiredProps,
-    SearchToolRequiredPropTypes,
-    SearchToolRequiredDefaultProps
-} from "../SearchToolRequiredProps";
-import { BaseMatch } from "../BaseMatch";
+import { Condition } from "../../CriteriaTable";
+import {BaseMatch, BaseMatchState} from "../BaseMatch";
 
-export interface StringMatchState {
-    searchValue: string;
-}
-
-export class StringMatch extends BaseMatch<SearchToolRequiredProps, StringMatchState> {
-    public static readonly propTypes = SearchToolRequiredPropTypes;
-    public static readonly contextTypes = CriteriaTableContextTypes;
-    public static readonly defaultProps = SearchToolRequiredDefaultProps;
-
-    public readonly context: CriteriaTableContext;
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            searchValue: ""
-        };
-    }
+export class StringMatch extends BaseMatch {
+    public state: BaseMatchState = {
+        searchValue: ""
+    };
 
     public render(): JSX.Element {
         return (
