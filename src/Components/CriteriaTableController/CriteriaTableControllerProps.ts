325e2e519d027e9c4c1f230b61386c2c46418776
Beton <aleksa.yagich@gmail.com>
2018-10-11 11:19:00 +0300

    Implement flag `enableCaching` for CriteriaTableController

--- src/Components/CriteriaTableController/CriteriaTableControllerProps.ts
+++ src/Components/CriteriaTableController/CriteriaTableControllerProps.ts
@@ -2,8 +2,16 @@ import * as PropTypes from "prop-types";
 
 export interface CriteriaTableControllerProps {
     onError?: (error: any) => void;
+    enableCaching?: boolean;
 }
 
 export const CriteriaTableControllerPropTypes: {[P in keyof CriteriaTableControllerProps]: PropTypes.Validator<any>} = {
-    onError: PropTypes.func
+    onError: PropTypes.func,
+    enableCaching: PropTypes.bool,
+};
+
+export const CriteriaTableControllerDefaultProps: {
+    [P in keyof CriteriaTableControllerProps]?: CriteriaTableControllerProps[P]
+} = {
+    enableCaching: true,
 };
